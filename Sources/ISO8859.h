2a1d621a5ec7ba6cd6bb7ceba459aa51f111dd52
Devran Ünal <me@devranuenal.com>
2016-12-05 19:54:21 +0100

    Create project with targets

---
+++ Sources/ISO8859.h
@@ -0,0 +1,19 @@
+//
+//  ISO8859.h
+//  ISO8859
+//
+//  Created by Devran Uenal on 5.12.16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ISO8859.
+FOUNDATION_EXPORT double ISO8859_VersionNumber;
+
+//! Project version string for ISO8859.
+FOUNDATION_EXPORT const unsigned char ISO8859VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ISO8859/PublicHeader.h>
+
+
